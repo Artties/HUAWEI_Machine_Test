3b5fd7d08fb378ca7e631d730ecb7f6691da3371
Artties <hecncn@126.com>
2021-02-19 18:42:31 +0800

    Update for first time

---
+++ Count_two_string.py
@@ -0,0 +1,7 @@
+#Count two words find how many times a exist in b
+#Line one input first word
+#Line two input second word
+#Print count results.
+a = input().lower()
+b = input().lower()
+print(a.count(b))
---
+++ Last_word_length.py
@@ -0,0 +1,3 @@
+#Return the last word length of the whole words
+str = input().strip().split()
+print(len(str[len(str)-1])
